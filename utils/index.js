b4f18d75468d51bc2c6338224ef04ba90646ea9b
Andre Regino <reginopanai@gmail.com>
2025-10-16 19:34:44 -0600

    utility function to verify user is logged in

--- utils/index.js
+++ utils/index.js
@@ -9,5 +9,17 @@ Util.handleErrors = fn => (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch(next)
 };
 
+Util.verifyUser = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) return res.status(401).json({ error: 'No token' });
+
+  try {
+    const decoded = jwt.verify(token, process.env.SUPABASE_JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(403).json({ error: 'Invalid token' });
+  }
+}
 
 module.exports = Util;
