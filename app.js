47c2050b4b5e4ceda059ffd0bb0038333de03cf8
Andre Regino <reginopanai@gmail.com>
2025-11-26 17:35:18 -0700

    updated cors policy

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(
   cors({
     origin: ['http://localhost:3000'],
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   })
 );
 /********************
